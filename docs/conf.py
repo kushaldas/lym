341f40725d80c3382583c86e37f1943f2b8f6fc4
Kushal Das <mail@kushaldas.in>
2022-03-24 13:45:11 +0100

    Updates year

--- docs/conf.py
+++ docs/conf.py
@@ -57,7 +57,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Linux command line for you and me'
-copyright = u'2017-2021, Kushal Das'
+copyright = u'2017-2022, Kushal Das'
 author = u'Kushal Das'
 
 # The version info for the project you're documenting, acts as replacement for
